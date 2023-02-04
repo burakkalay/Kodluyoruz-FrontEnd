433f262234610a4f8adaa0a053cd33ecd47f02bb
Burak Kalay <burak9.kalay@gmail.com>
2023-02-04 16:40:29 +0300

    clock.js revised

--- JavaScript/Odev_1/js/clock.js
+++ JavaScript/Odev_1/js/clock.js
@@ -8,7 +8,7 @@ const days = ["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartes
 
 // fonksiyonumuzu olusturup icerisine gerekli islemleri yazalim.
 function showTime(){
-    // Suanki zamanı tanimlayalim.
+    // Suanki zamani tanimlayalim.
     let currentDate = new Date();
 
     // Saat, Dakika ve Saniyeleri tanimlayalim.
@@ -19,15 +19,27 @@ function showTime(){
     // Gunleri tanimyalip day degiskenine atayalim.
     let day = days[currentDate.getDay()];
 
+    // Tam tarih
+    let date = currentDate.getDate();
+
+    // Ayi ve yili tanimlayalim
+    let month = currentDate.getMonth() + 1;
+    let year = currentDate.getFullYear();
+
     // Eger saat, dakika ve saniye 10 dan kucukse basina 0 ekleyerek yazdirsin, degilse degistirmeden aynen yazsin.
     hours = (hours<10) ? `0${hours}`: hours;
     minutes = (minutes<10) ? `0${minutes}`: minutes;
     seconds = (seconds<10) ? `0${seconds}`: seconds;
 
+    // Eger gün ve ay 10 dan kücükse basina 0 ekleyerek yazdirsin.
+    date = (date<10) ? `0${date}`: date;
+    month = (month<10) ? `0${month}`: month;
+
     // Ekrana yazdirilacak zaman formati
     var timeString = `${hours}:${minutes}:${seconds}`;
-    // myClock isimli id'yi cagirip innerHTML ile içine saati ve gunu yazdiralim.
-    document.getElementById("myClock").innerHTML = `${timeString} ${day}`;
+    var dateString = `${day} ${month}/${year}`;
+    // myClock isimli id'yi cagirip innerHTML ile içine saati, tarihi ve gunu yazdiralim.
+    document.getElementById("myClock").innerHTML = `${date}/${month}/${year} ${day} Günü, Saat ${timeString}`;
 }
 // setInterval() komutu ile fonksiyonumuzu her 1sn (1000ms) de bir surekli calistiralim.
 setInterval(showTime, 1000);
